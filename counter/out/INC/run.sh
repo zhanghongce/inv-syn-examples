788c1a42901d363b17fff65617405115c3ea12dc
Hongce Zhang <zhanghongce@126.com>
2019-02-24 19:12:47 -0500

    Update run.sh

--- counter/out/INC/run.sh
+++ counter/out/INC/run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-yosys gensmt.ys
+# you can run the following to generate SMT (assume you have yosys installed)
+yosys __gen_smt_script.ys 
+# you can run the following to invoke Z3 CHC solver
+z3 wrapper.smt2
+
